739d8d8036c429f100ad6251abf0b9801986464a
joao3370 <79807279+joao3370@users.noreply.github.com>
2021-12-02 00:14:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1930,7 +1930,7 @@ if (isLimit(sender)) return webPage(linguagem.cash_adquirir(pushname, p), {quote
 if (!isGroup) return enviar(linguagem.group())
 const total_gp1 = `${totalchat.length}`
 const grupo_by_bdr = (linguagem.menu2(pushname, hr, total_gp1, bateria))
-wew = fs.readFileSync('./base de dados/lib/bot/fotos/Grupo.jpg.mp4')
+wew = fs.readFileSync('./base de dados/lib/bot/fotos/Grupo.jpg')
 bdr.sendMessage(from, wew, video, {quoted: mek, mimetype: Mimetype.gif, thumbnail:null, caption: grupo_by_bdr})
 await requestCash(sender)
 break
@@ -3648,11 +3648,10 @@ addFilter(from)
 if (isLimit(sender)) return webPage(linguagem.cash_adquirir(pushname, p), {quoted: mek}) 
 const total_gp5 = `${totalchat.length}`
 const jogos_by_bdr = (linguagem.jogos(pushname, hr, total_gp5, bateria))
-wew = fs.readFileSync('./base de dados/lib/bot/fotos/Jogos.jpg')
+wew = fs.readFileSync('./base de dados/lib/bot/fotos/JogosFT.jpg')
 bdr.sendMessage(from, wew, video, {quoted: mek, mimetype: Mimetype.gif, thumbnail:null, caption: jogos_by_bdr})
 await requestCash(sender)
 break
-
 case 'preto':
 case 'feio':
 case 'lixo':
